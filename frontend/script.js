e9357ef50851a6f70e8ba7780e89d8993c2c90dd
Sriram Arjun <sriramarjun2007@gmail.com>
2026-05-30 22:13:01 +0530

    Futuristic showroom details with popup

--- frontend/script.js
+++ frontend/script.js
@@ -29,9 +29,12 @@ function popupscreen1(){
     document.getElementById("overlay").style.display = "flex";
     var money1=document.getElementById("moneypop");
     money1.textContent="$1,899";
-    money1.style.fontWeight="1500";
-    money1.style.fontSize="20px"; 
-    money1.style.color="skyblue"; 
+    money1.style.background = "linear-gradient(to right, white, #00C4CC)";
+    money1.style.webkitBackgroundClip = "text";
+    money1.style.webkitTextFillColor = "transparent";
+    money1.style.fontFamily = "Poppins, sans-serif";
+    money1.style.fontWeight="1000";
+    money1.style.fontSize="25px"
     var productname1=document.getElementById("productpop");
     productname1.textContent = "Aurora SpaceBook";
     productname1.style.color="white";
@@ -42,11 +45,14 @@ function popupscreen2(){
     document.getElementById("overlay").style.display = "flex";
    var money2=document.getElementById("moneypop");
     money2.textContent="$3,299";
-    money2.style.fontWeight="1500";
-    money2.style.fontSize="20px"; 
-    money2.style.color="skyblue"; 
+    money2.style.background = "linear-gradient(to right, white, #00C4CC)";
+    money2.style.webkitBackgroundClip = "text";
+    money2.style.webkitTextFillColor = "transparent";
+    money2.style.fontFamily = "Poppins, sans-serif";
+    money2.style.fontWeight="1000";
+    money2.style.fontSize="25px"
     var productname2=document.getElementById("productpop");
-    productname2.textContent = "Nebula Titan X";
+    productname2.textContent ="Nebula Titan X";
     productname2.style.color="white";
     productname2.style.fontWeight="1000";
     productname2.style.fontSize="20px"
@@ -54,11 +60,14 @@ function popupscreen2(){
 
 function popupscreen3(){
     document.getElementById("overlay").style.display = "flex";
-   var money3=document.getElementById("moneypop");
+    var money3=document.getElementById("moneypop");
     money3.textContent="$2,499";
-    money3.style.fontWeight="1500";
-    money3.style.fontSize="20px"; 
-    money3.style.color="skyblue"; 
+    money3.style.background = "linear-gradient(to right, white, #00C4CC)";
+    money3.style.webkitBackgroundClip = "text";
+    money3.style.webkitTextFillColor = "transparent";
+    money3.style.fontFamily = "Poppins, sans-serif";
+    money3.style.fontWeight="1000";
+    money3.style.fontSize="25px"
     var productname3=document.getElementById("productpop");
     productname3.textContent = "Stellar Pro-X";
     productname3.style.color="white";
